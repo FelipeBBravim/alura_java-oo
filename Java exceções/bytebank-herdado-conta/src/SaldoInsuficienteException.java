26b38371057518351b2304429750ea038e932dde
FelipeBBravim <felipe.bravim@gmail.com>
2021-02-09 12:04:11 -0300

    Sacando com Unchecked Exception

---
+++ Java exceções/bytebank-herdado-conta/src/SaldoInsuficienteException.java
@@ -0,0 +1,9 @@
+
+public class SaldoInsuficienteException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public SaldoInsuficienteException(String msg) {
+		super(msg);
+	}
+	
+}
