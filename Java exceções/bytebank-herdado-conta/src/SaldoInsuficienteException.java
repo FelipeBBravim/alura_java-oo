08cf9e57260e99e6dd874c496f0586554e626a62
FelipeBBravim <felipe.bravim@gmail.com>
2021-02-09 13:34:21 -0300

    Sacando com Checked Exception

--- Java exceções/bytebank-herdado-conta/src/SaldoInsuficienteException.java
+++ Java exceções/bytebank-herdado-conta/src/SaldoInsuficienteException.java
@@ -1,5 +1,5 @@
 
-public class SaldoInsuficienteException extends RuntimeException {
+public class SaldoInsuficienteException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public SaldoInsuficienteException(String msg) {
--- Java exceções/bytebank-herdado-conta/src/TesteSaca.java
+++ Java exceções/bytebank-herdado-conta/src/TesteSaca.java
@@ -7,9 +7,13 @@ public class TesteSaca {
 		
 		conta.deposita(200.0);
 		
-		conta.saca(210.0);
+		try {
+			conta.saca(210.0);
+		} catch(SaldoInsuficienteException e) {
+			System.out.println(e.toString());
+		}
 		
-		System.out.println(conta.getSaldo());
+		System.out.println("Saldo: " + conta.getSaldo());
 	}
 
 }
